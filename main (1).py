eb1c67f1c7b2a2f9449d474d6a09e006e4ed5cf7
Abhishek Pal <68822024+Abhishek2717@users.noreply.github.com>
2021-06-27 23:36:47 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,54 @@
+import smtplib
+import speech_recognition as sr
+import pyttsx3
+from email.message import EmailMessage
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+def get_info():
+    try:
+        with sr.Microphone() as source:
+            print('listening..')
+            voice = listener.listen(source)
+            info = listener.recognize_google(voice)
+            print(info)
+            return info.lower()
+
+    except:
+        pass
+
+def send_email(receiver, subject, message):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('isshuekumar@gmail.com', 'password')
+    email = EmailMessage()
+    email['From'] = 'rushikasabharwa1@gmail.com'
+    email['To'] = receiver
+    email['Subject'] = subject
+    email.set_content(message)
+    server.send_message(email)
+
+
+
+email_list = {
+    'rushika':'rushikasabharwal01@gmail.com',
+    'Abhishek': 'isshuekumar@gmail.com',
+    'lisa':'lisa@blackpink.com',
+
+}
+
+def get_email_info():
+    talk('To Whome you want to send email')
+    name = get_info()
+    receiver = email_list[name]
+    print(receiver)
+    talk('What is the subject of your email?')
+    subject = get_info()
+    talk('Tell me the text in your email')
+    message = get_info()
+    send_email(receiver, subject)
+
+get_email_info()
